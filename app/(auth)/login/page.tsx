8ffdafc1abc3b075c357a186b024a4c623466baa
usama-pixel <usamaali052@gmail.com>
2023-12-29 02:28:38 +0500

    first commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+type Props = {}
+
+const page: FC<Props> = ({}) => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
---
+++ app/home/page.tsx
@@ -0,0 +1,14 @@
+import Carousel from '@/components/Carousel'
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div>
+        <Carousel />
+    </div>
+  )
+}
+
+export default page
